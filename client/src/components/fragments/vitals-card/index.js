8fbd1a44f0947586d13d809adbb3f301477b8f57
Balu <edesbalazs@gmail.com>
2017-01-02 18:45:57 +0000

    fixed charts

--- client/src/components/fragments/vitals-card/index.js
+++ client/src/components/fragments/vitals-card/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
-//import { } from 'victory-core'
-import { VictoryChart, VictoryLine } from 'victory-chart'
 import last from 'mini-dash/last'
 import isDefined from 'mini-dash/isDefined'
+import { ResponsiveContainer, LineChart, Line, Tooltip, YAxis } from 'recharts'
 
 import { Card } from '../../ui/card'
 import { Icon } from '../../ui/icon'
 
-import { dataContainer, vitalsIcon, textContainer, valueText, } from './style'
+import { dataContainer, graphContainer, vitalsIcon, textContainer, valueText, noDataText } from './style'
 
 const unitNames = {
   CENTIMETRES: 'cm',
@@ -19,8 +18,17 @@ const unitNames = {
 
 const stringify = ({value, unit}) => (isDefined(value) && isDefined(unit)) ? `${value}${unitNames[unit]}` : 'No data'
 
-export const VitalsCard = ({ icon, name, data}) => {
+const NoChart = () => (<div className={noDataText}>No data</div>)
 
+const VitalsChart = ({data}) => (<ResponsiveContainer height={60}>
+  <LineChart data={data} >
+    <YAxis type='number' dataKey='value' domain={['dataMin', 'dataMax']} hide />
+    <Line type="monotone" dataKey="value" stroke="#666" />
+    <Tooltip labelStyle={{ fontFamily: 'Raleway' }} itemStyle={{ fontFamily: 'Raleway' }} />
+  </LineChart>
+</ResponsiveContainer>)
+
+export const VitalsCard = ({icon, name, data}) => {
   const text = stringify(last(data) || {})
 
   return <Card columns={1}>
@@ -31,6 +39,9 @@ export const VitalsCard = ({ icon, name, data}) => {
         <span className={valueText}>{text}</span>
       </div>
       <div style={{ clear: 'both' }}></div>
+    </div>
+    <div className={graphContainer}>
+      {data && data.length ? <VitalsChart data={data} /> : <NoChart />}
     </div>
   </Card>
 }
